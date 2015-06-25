e3c4a21082d4b0fa4ed566600d71d823de69040c
Jeannie <jeannie@kickpunchcreative.com>
2015-06-25 15:31:29 -0500

    event create form

--- controllers/events.rb
+++ controllers/events.rb
@@ -6,6 +6,12 @@ get "/create_event/:x" do
   erb :"create_event"
 end
 
+get "save_event" do
+  @event = Event.new({"name" => params["name"], "date" => params["date"], "category_id" => params["category_id"], "reminder_time" => params["reminder_time"], "comment" => params["comment"]})
+  @event.add_to_database
+      erb :"add_owner_success"
+end
+
 # edit event -------------------------------------------------
 
 
