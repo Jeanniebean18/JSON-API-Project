57681ec6cbc5f0a822e0b2cf10baed81f74a614c
Jeannie <jeannie@kickpunchcreative.com>
2015-06-25 16:00:26 -0500

    create event form

--- controllers/events.rb
+++ controllers/events.rb
@@ -1,15 +1,16 @@
 # events controller
 
 # add event------------------------------------------------------
-get "/create_event/:x" do
+get "/create_event/:x/:y" do
   @category = Category.find(params["x"])
+  @owner = Owner.find(params["y"])
   erb :"create_event"
 end
 
 get "save_event" do
   @event = Event.new({"name" => params["name"], "date" => params["date"], "category_id" => params["category_id"], "reminder_time" => params["reminder_time"], "comment" => params["comment"]})
   @event.add_to_database
-      erb :"add_owner_success"
+      erb :"save_event_success"
 end
 
 # edit event -------------------------------------------------
